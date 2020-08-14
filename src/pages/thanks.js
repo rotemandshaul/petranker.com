456af8560c62a64f84cf7338a7f720077dd0a8c0
borndead <shaulitto1@gmail.com>
2020-08-15 01:14:24 +0200

    basic thanks page

---
+++ src/pages/thanks.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Layout from '../components/Layout/layout';
+
+const ThanksPage = () => <Layout>Thank you for contacting Rotem</Layout>;
+
+export default ThanksPage;
