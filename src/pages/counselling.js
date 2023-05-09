f6b8ec03ea819963c1e34b92ea904d3432d107ec
rotemandshaul <112261253+rotemandshaul@users.noreply.github.com>
2023-05-09 22:14:12 +0200

    Update counselling.js

--- src/pages/counselling.js
+++ src/pages/counselling.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { headData } from '../mock/data';
-import Layout from '../components/Layout/layout';
-import Counselling from '../components/Counselling/Counseling';
-import ContactForm from '../components/Contact/ContactForm';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../style/main.scss';
+// import React from 'react';
+// import { Helmet } from 'react-helmet';
+// import { headData } from '../mock/data';
+// import Layout from '../components/Layout/layout';
+// import Counselling from '../components/Counselling/Counseling';
+// import ContactForm from '../components/Contact/ContactForm';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../style/main.scss';
 
-const Counsellingpage = () => {
-  const { lang } = headData;
-  const { title, description } = headData.counseling;
+// const Counsellingpage = () => {
+//   const { lang } = headData;
+//   const { title, description } = headData.counseling;
 
-  return (
-    <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{title}</title>
-        <html lang={lang} />
-        <meta name="description" content={description} />
-      </Helmet>
-      <Layout>
-        <Counselling />
-        <div>
-          <h1 className="hero-info-text">Directly Book With Rotem:</h1>
-        </div>
-        <ContactForm />
-      </Layout>
-    </>
-  );
-};
-export default Counsellingpage;
+//   return (
+//     <>
+//       <Helmet>
+//         <meta charSet="utf-8" />
+//         <title>{title}</title>
+//         <html lang={lang} />
+//         <meta name="description" content={description} />
+//       </Helmet>
+//       <Layout>
+//         <Counselling />
+//         <div>
+//           <h1 className="hero-info-text">Directly Book With Rotem:</h1>
+//         </div>
+//         <ContactForm />
+//       </Layout>
+//     </>
+//   );
+// };
+// export default Counsellingpage;
